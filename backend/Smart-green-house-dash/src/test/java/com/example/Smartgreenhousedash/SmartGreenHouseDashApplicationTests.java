14264a23de0f69fbbdb7dcad8b9dea05d941c1b0
MyriamBA <mariam.benahmed@supcom.tn>
2022-02-15 13:18:19 +0100

    Backend

---
+++ backend/Smart-green-house-dash/src/test/java/com/example/Smartgreenhousedash/SmartGreenHouseDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Smartgreenhousedash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGreenHouseDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
